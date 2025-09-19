a50bdf90c9aef863ee43fa930d5039f5d38ebfcc
De'Jour <dejourford@yahoo.com>
2025-09-19 08:55:58 -0500

    hide/show modal

--- src/tabs.js
+++ src/tabs.js
@@ -2,6 +2,70 @@
 const ls = localStorage
 const pillNavButtons = document.querySelectorAll(".pill-item")
 
+// CREATE INPUT FORM FOR NEW TASK AND APPEND TO APP
+const addNewTaskModal = `
+<div id="addNewTaskModal" class="modal">    
+    <form class="new-task-modal">
+        <h2 class="form-title">Add New Task</h2>
+        <section class="form-data">
+            <div class="input-item" id="title">
+                <label for="taskTitle">Title</label>
+                <input id="taskTitle" name="title">
+            </div>
+            <div class="input-item" id="description">
+                <label for="taskDesc">Description</label>
+                <input id="taskDesc" name="description">
+            </div>
+            <div class="input-item" id="date">
+                <label for="Due Date">Due Date</label>
+                <input id="taskDate" name="due" type="date">
+            </div>
+            <div class="input-item" id="priority">
+                <label for="taskPriority">Priority</label>
+                <select id="taskPriority" name="priority">
+                    <option value="Low">Low</option>
+                    <option value="Medium">Medium</option>
+                    <option value="High">High</option>
+                </select>
+            </div>
+            <div class="input-item" id="project">
+                <label for="taskProject">Project</label>
+                <select id="taskProject" name="project">
+                    <option value="inbox">Inbox</option>
+                </select>
+            </div>
+            <div class="input-item" id="notes">
+                <label for="taskNotes">Notes</label>
+                <textarea id="taskNotes" name="notes"></textarea>
+            </div>
+        </section>
+        <div class="form-buttons">
+            <button type="button" id="cancelNewTask">Cancel</button>
+            <button type="submit">Confirm</button>
+        </div>
+    </form>
+</div>    
+`
+
+// append form to app
+app.insertAdjacentHTML("beforeend", addNewTaskModal)
+const modal = document.querySelector("#addNewTaskModal")
+const form = modal.querySelector("form")
+const cancelBtn = document.querySelector("#cancelNewTask")
+
+// function to open new task modal
+function openNewTaskModal() {
+    modal.classList.add("active")
+    modal.setAttribute("aria-hidden", false)
+}
+
+// function to close new task modal
+function closeNewTaskModal() {
+    modal.classList.remove("active")
+    modal.setAttribute("aria-hidden", true)
+}
+
+
 // set active tab function when clicked
 function setActiveTab(name) {   
 
@@ -19,66 +83,12 @@ function setActiveTab(name) {
 function handleClick(e) {
     const name = e.currentTarget.dataset.tab
     setActiveTab(name)
+
+    if (name === "New Task") {
+        openNewTaskModal()
+    }
 }
 
-// CREATE INPUT FORM FOR NEW TASK AND APPEND TO APP
-const addNewTaskModal = `
-<form class="new-task-modal">
-    <h2 class="form-title"></h2>
-    <section class="form-data">
-        <div class="input-item" id="title">
-            <label for="Title">Title</label>
-            <input>
-        </div>
-        <div class="input-item" id="description">
-            <label for="Description">Description</label>
-            <input>
-        </div>
-        <div class="input-item" id="date">
-            <label for="Due Date">Due Date</label>
-            <input type="date">
-        </div>
-        <div class="input-item" id="priority">
-            <label for="Priority">Priority</label>
-            <select>
-                <option value="Low">Low</option>
-                <option value="Medium">Medium</option>
-                <option value="High">High</option>
-            </select>
-        </div>
-        <div class="input-item" id="project">
-            <label for="Project">Project</label>
-            
-        </div>
-        <div class="input-item" id="notes">
-            <label for="Notes">Notes</label>
-            <textarea name="notes"></textarea>
-        </div>
-    </section>
-    <div class="form-buttons">
-        <button type="button">Cancel</button>
-        <button type="submit">Confirm</button>
-    </div>
-</form>
-`
-
-
-// form + title 
-const form = document.createElement("form")
-const formTitle = document.createElement("h2")
-formTitle.classList.add("form-title")
-formTitle.textContent = "Add New Task"
-
-// title input
-const titleInput = document.createElement("input")
-titleInput
-
-// append form title 
-form.append(formTitle)
-
-
-// append form to app
-app.insertAdjacentHTML("beforeend", addNewTaskModal)
 
 // pill nav listeners
 pillNavButtons.forEach((button) => {
